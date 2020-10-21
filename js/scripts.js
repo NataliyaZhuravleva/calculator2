188ac3a9fd09371950109c534d6f2277b7a3a608
Tyler Sinks <tyler.sinks@gmail.com>
2020-10-21 18:58:32 -0400

    add user interface logic for add funcgtion  and remove alerts
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,19 @@ function divide(number1, number2){
   return number1/number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result1 = add(number1, number2);
-const result2 = subtract(number1, number2);
-const result3 = multiply(number1, number2);
-const result4 = divide(number1,number2)
-alert(result1, result2, result3, result4);
-alert(result4);
+// const result1 = add(number1, number2);
+// const result2 = subtract(number1, number2);
+// const result3 = multiply(number1, number2);
+// const result4 = divide(number1,number2)
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  })
+});
+// alert(result1, result2, result3, result4);
+// alert(result4);
